ff46f0510e1c116d9d4e2b061286f596ac7c1b47
Sam Couch <samcouch91@gmail.com>
2015-09-09 20:48:14 -0400

    all around refactor

---
+++ src/index.js
@@ -0,0 +1,72 @@
+var Path = require('path')
+var Request = require('request')
+var URL = require('url')
+var Util = require('util')
+
+/**
+ * A simple interface to the Slack web API
+ * @class
+ * @param {string} token
+ */
+var PrettySlack = module.exports =  function(token){
+  /** @member {string} */
+  this.token = token
+  this.__uri = {
+    protocol: 'https',
+    hostname: 'slack.com',
+    pathname: '/api'
+  }
+}
+
+/**
+ * Interface to the `chat` API method.
+ * @param {string} channel
+ * @param {string} message
+ * @param {object} options
+ * @param {function} callback
+ */
+PrettySlack.prototype.chat = function(channel, message, options, callback){
+  var payload = {
+    channel: channel,
+    text: message,
+    icon_emoji: options.icon_emoji,
+  }
+
+  if (options.attachments instanceof Array)
+    payload.attachments = JSON.stringify(options.attachments)
+
+  this.api("chat.postMessage", payload, callback);
+}
+
+/**
+ * Creates URI for the specific API method
+ * @param {string} method
+ * @param {object} query
+ * @return {string}
+ */
+PrettySlack.prototype.uri = function(method, query){
+  var u = Util._extend({}, this.__uri);
+  u.pathname = Path.join(u.pathname, method)
+  u.query = query || {}
+  u.query.token = this.token
+
+  return URL.format(u)
+}
+
+/**
+ * Makes the request to Slack
+ * @param {string} method
+ * @param {object} request
+ * @param {function} callback
+ */
+PrettySlack.prototype.api = function(method, request, callback){
+  Request.get({
+    url: this.uri(method, request)
+  }, function(err, res, body){
+    if (callback instanceof Function){
+      var resp = JSON.parse(body)
+      callback(err, (resp.ok === true), resp)
+    }
+  })
+}
+
